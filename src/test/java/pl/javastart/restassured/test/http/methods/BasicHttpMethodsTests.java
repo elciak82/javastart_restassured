25776f4f9a4b828cf1e75ad48d3dde5e93c8e002
elciak82 <elciak82@gmail.com>
2021-03-07 13:41:42 +0100

    givenPetWhenPostPetThenPetIsCreatedTest - POJO refactor

--- src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
@@ -1,6 +1,11 @@
 package pl.javastart.restassured.test.http.methods;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.Category;
+import pl.javastart.main.pojo.Pet;
+import pl.javastart.main.pojo.Tag;
+
+import java.util.Collections;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,24 +13,21 @@ public class BasicHttpMethodsTests {
 
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
-        String pet = "{\n" +
-                "  \"id\": 111,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"wolf\"\n" +
-                "  },\n" +
-                "  \"name\": \"wolfie\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"https://photos.com/wolf1.jpg\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 0,\n" +
-                "      \"name\": \"wolf-category\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
+
+        Category category = new Category();
+        category.setId(1);
+        category.setName("wolf");
+
+        Tag tag = new Tag();
+        tag.setId(0);
+        tag.setName("wolf-category");
+
+        Pet pet = new Pet();
+        pet.setId(111);
+        pet.setCategory(category);
+        pet.setPhotoUrls(Collections.singletonList("https://photos.com/wolf1.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("available");
 
         given().log().all().body(pet).contentType("application/json")
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
