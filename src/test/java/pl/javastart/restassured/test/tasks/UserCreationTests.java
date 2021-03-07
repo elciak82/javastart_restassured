ee76f689a5aa182506854d83b4d0fa78213d7c89
elciak82 <elciak82@gmail.com>
2021-03-07 14:04:57 +0100

    new test givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest

--- src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -32,4 +32,30 @@ public class UserCreationTests {
                 .when().get("http://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
                 .then().log().all().statusCode(200);
     }
+
+    @Test
+    public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
+
+        User user = new User();
+        user.setId(446);
+        user.setUsername("secondUser");
+        user.setFirstName("Dariusz");
+        user.setLastName("Zabawny");
+        user.setEmail("jestemZabawny@test.com");
+        user.setPassword("password");
+        user.setPhone("+123456789");
+        user.setUserStatus(1);
+
+        given().log().all()
+                .contentType("application/json")
+                .body(user)
+                .when().post("http://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .then().log().all().statusCode(200);
+
+        given().log().all()
+                .contentType("application/json")
+                .pathParam("username", user.getUsername())
+                .when().get("http://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .then().log().all().statusCode(200);
+    }
 }
