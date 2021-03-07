a3a90f705e6ed7f229d0f1e4a72981e0e87eba1a
elciak82 <elciak82@gmail.com>
2021-03-07 13:20:06 +0100

    POJO

--- src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
+++ src/test/java/pl/javastart/restassured/test/http/methods/BasicHttpMethodsTests.java
@@ -39,4 +39,86 @@ public class BasicHttpMethodsTests {
                 .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
                 .then().log().all().statusCode(200);
     }
+
+    @Test
+    public void givenExistingPetWhenUpdatePetNameThenPetIsChangedTest() {
+
+        String pet = "{\n" +
+                "  \"id\": 111,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"wolf\"\n" +
+                "  },\n" +
+                "  \"name\": \"wolfie\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"https://photos.com/wolf1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"wolf-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+
+        pet = "{\n" +
+                "  \"id\": 111,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"Wilk\"\n" +
+                "  },\n" +
+                "  \"name\": \"wolfie\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"https://photos.com/wolf1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"wolf-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+    }
+
+    @Test
+    public void givenExistingPetIdWhenDeletingPetThenIsDeletedTest() {
+
+        String pet = "{\n" +
+                "  \"id\": 111,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 1,\n" +
+                "    \"name\": \"Wilk\"\n" +
+                "  },\n" +
+                "  \"name\": \"wolfie\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"https://photos.com/wolf1.jpg\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"wolf-category\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+
+        given().log().all().contentType("application/json")
+                .pathParam("petId", 111)
+                .when().delete("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/{petId}")
+                .then().log().all().statusCode(200);
+    }
 }
